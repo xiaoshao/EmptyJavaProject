59287050f0a653db2a67c8fa21d7c7816d06a7c2
psw-jade team <psw-jade@rea.com>
2016-09-22 14:05:49 +0800

    Init a purely spring mvc project.

--- src/main/java/com/controller/FistController.java
+++ src/main/java/com/controller/FistController.java
@@ -1,10 +1,9 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
-import java.util.Map;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FistController {
